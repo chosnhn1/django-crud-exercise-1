b10ebadc3b25665252e5ae80dee6dee838c7d8d9
Cho Seonghan <chosnhn1@gmail.com>
2023-02-27 21:18:09 +0900

    accounts view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from accounts.serializers import UserSerializer
 from rest_framework import status
+from accounts.models import User
+
 
 # Create your views here.
 # class UserList(APIView):
@@ -23,10 +25,24 @@ class UserAPI(APIView):
 
         return
     
-    def patch(self, request):
+    def patch(self, request, pk):
+        user = get_object_or_404(User, pk=request.pk)
+        serializer = UserSerializer(user, data=request.data)
+        if request.user.pk == pk or request.user.is_admin:
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
         # change user profile
         return
 
-    def delete(self, request):
-        #sign off
-        return
+    def delete(self, request, pk):
+        sign_off_user = get_object_or_404(User, pk=request.user.pk)
+        if request.user.pk == pk or request.user.is_admin:
+            sign_off_user.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        
